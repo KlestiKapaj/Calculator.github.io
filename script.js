02693555b08bb47184f550cd6e350a3908bcb336
KlestiKapaj <162467857+KlestiKapaj@users.noreply.github.com>
2024-08-30 09:38:01 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const display = document.getElementById("display");
+
+function append_to_display(input) {
+    display.value += input;
+}
+
+function clearDisplay() {
+    display.value = "";
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = "Error";
+    }
+}
